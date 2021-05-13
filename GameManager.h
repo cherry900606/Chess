c6810da056f965289621e0f6583242a43d777f97
chuichenguo <49388851+chuichenguo@users.noreply.github.com>
2021-05-13 17:49:30 +0800

    Add files via upload

---
+++ GameManager.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Board.h"
+#include "Player.h"
+#include "Viewer.h"
+#include <vector>
+#include <fstream>
+
+
+class GameManager {
+public:
+	Player* players[2]; // human player or ai player
+	int current_player; // 現在可以移動旗子的玩家
+	Board board; // now board status
+	Viewer viewer; // show the board
+	vector<string> saveFile;
+
+	GameManager(); // initialsize
+	void game(Board board); // game loop
+	void showMenu();
+	bool invalidMove(Position moveFromPos, Position moveToPos, int type, Board& board);
+	bool isCheckmate(Board board, bool kingIsWhite);
+	bool Castling(Position moveFromPos, Position moveToPos, Board board);
+};
